b5837ccd7937598ace45579828aa761ca694506e
Erik Storås Sommer <eriksommer99@gmail.com>
2020-03-26 00:09:39 +0100

    Lagt til dialogbokser

---
+++ src/main/java/org/semesteroppgave/gui/Dialogs.java
@@ -0,0 +1,24 @@
+package org.semesteroppgave.gui;
+
+import javafx.scene.control.Alert;
+
+public class Dialogs {
+
+    public static void showErrorDialog(String errorTitle, String errorHeader, String errorContent) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(errorTitle);
+        alert.setHeaderText(errorHeader);
+        alert.setContentText(errorContent);
+
+        alert.showAndWait();
+    }
+
+    public static void showSuccessDialog(String successTitle, String successHeader, String successContent) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(successTitle);
+        alert.setHeaderText(successHeader);
+        alert.setContentText(successContent);
+
+        alert.showAndWait();
+    }
+}
