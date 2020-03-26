b8308e6dfb7bbb77a3f079f441d3be5c07747660
Erik Storås Sommer <eriksommer99@gmail.com>
2020-03-26 17:47:54 +0100

    Lagt til table edit

--- src/main/java/org/semesteroppgave/gui/AdminComponentController.java
+++ src/main/java/org/semesteroppgave/gui/AdminComponentController.java
@@ -4,8 +4,10 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.input.KeyEvent;
+import org.semesteroppgave.Component;
 import org.semesteroppgave.Main;
 
 import java.io.IOException;
@@ -16,13 +18,13 @@ public class AdminComponentController {
     private Label lblAdminID;
 
     @FXML
-    private TableView<?> tableViewComponents;
+    private TableView<Component> tableViewComponents;
 
     @FXML
-    private ChoiceBox<?> cbFilter;
+    private ChoiceBox<String> cbFilter;
 
     @FXML
-    private ChoiceBox<?> cbCreate;
+    private ChoiceBox<String> cbCreate;
 
     @FXML
     void btnCreate(ActionEvent event) throws IOException {
@@ -42,6 +44,21 @@ public class AdminComponentController {
 
     @FXML
     void txtSearch(KeyEvent event) {
+
+    }
+
+    @FXML
+    void editModel(TableColumn.CellEditEvent<Component, String> event) {
+
+    }
+
+    @FXML
+    void editPrice(TableColumn.CellEditEvent<Component, Double> event) {
+
+    }
+
+    @FXML
+    void editVersion(TableColumn.CellEditEvent<Component, String> event) {
 
     }
 }
