177df287b9d172ddbb84ff9ceaeae30b1b952756
Erik Storås Sommer <eriksommer99@gmail.com>
2020-03-31 21:36:47 +0200

    Gjort klart til møte

--- src/main/java/org/semesteroppgave/gui/PrimaryController.java
+++ src/main/java/org/semesteroppgave/gui/PrimaryController.java
@@ -6,48 +6,11 @@ import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import org.semesteroppgave.Context;
 import org.semesteroppgave.Main;
 import org.semesteroppgave.carcomponents.*;
 
-public class PrimaryController implements Initializable {
-
-    //Dette er bare for testing
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        Motor motor = new Motor("V12",12300,"universial");
-        Context.getInstance().getRegisterComponent().setComponentsList(motor);
-        Motor motor2 = new Motor("V8",1230,"universial");
-        Context.getInstance().getRegisterComponent().setComponentsList(motor2);
-        Rim rim = new Rim("Feit",3900,"universial");
-        Context.getInstance().getRegisterComponent().setComponentsList(rim);
-        SeatCover seatCover = new SeatCover("Mykt",123,"universial");
-        Context.getInstance().getRegisterComponent().setComponentsList(seatCover);
-        SeatCover seatCover2 = new SeatCover("Sport",12300,"universial");
-        Context.getInstance().getRegisterComponent().setComponentsList(seatCover2);
-        Spoiler spoiler = new Spoiler("Høy",1000,"universial");
-        Context.getInstance().getRegisterComponent().setComponentsList(spoiler);
-        Spoiler spoiler2 = new Spoiler("Lav",500,"universial");
-        Context.getInstance().getRegisterComponent().setComponentsList(spoiler2);
-        Tires tires = new Tires("Vinter",1000,"universial");
-        Context.getInstance().getRegisterComponent().setComponentsList(tires);
-        Tires tires2 = new Tires("Sommer",1500,"universial");
-        Context.getInstance().getRegisterComponent().setComponentsList(tires2);
-        Tires tires3 = new Tires("Alround",2000,"universial");
-        Context.getInstance().getRegisterComponent().setComponentsList(tires3);
-        Gearbox gearbox = new Gearbox("Manuell",3000,"Diesel");
-        Context.getInstance().getRegisterComponent().setComponentsList(gearbox);
-        Gearbox gearbox2 = new Gearbox("Automat",5000,"Diesel");
-        Context.getInstance().getRegisterComponent().setComponentsList(gearbox2);
-        Battery battery = new Battery("Li-ion",10000,"Hybrid", "Electric");
-        Context.getInstance().getRegisterComponent().setComponentsList(battery);
-        FuelContainer fuelContainer = new FuelContainer("50-liter",5000,"Diesel", "Hybrid");
-        Context.getInstance().getRegisterComponent().setComponentsList(fuelContainer);
-        FuelContainer fuelContainer2 = new FuelContainer("100-liter",10000,"Diesel", "Hybrid");
-        Context.getInstance().getRegisterComponent().setComponentsList(fuelContainer2);
-
-    }
+public class PrimaryController {
 
     @FXML
     void btnAdmin(ActionEvent event) throws IOException {
